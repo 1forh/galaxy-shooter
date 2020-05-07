f656247fd676983310dc4ebd16718e1241be7527
1forh <1forhlol@gmail.com>
2020-05-07 17:58:51 -0400

    inits

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+
+    void InitPlayerControls()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float x = horizontalInput * _speed * Time.deltaTime;
+        float y = verticalInput * _speed * Time.deltaTime;
+        Vector3 position = new Vector3(x, y, 0);
+        transform.Translate(position);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InitPlayerControls();
+
+        if (transform.position.y >= 3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, 3.8f, transform.position.z);
+        } else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, transform.position.z);
+        }
+
+
+        if (transform.position.x >= 11f)
+        {
+            transform.position = new Vector3(-11f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= -11f)
+        {
+            transform.position = new Vector3(11f, transform.position.y, transform.position.z);
+        }
+
+        // if player position on y is greater than 0,
+        // than y position = 0
+
+    }
+}
