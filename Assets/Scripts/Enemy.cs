0a6941b3002d4ab4ca6150317b32492dd8b4eda3
1forh <1forhlol@gmail.com>
2020-05-09 02:53:23 -0400

    updates

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bool isPlayer = other.tag == "Player";
+        bool isLaser = other.tag == "Laser";
+
+
+        
+        if (isPlayer)
+        {
+            // damage the player
+            Destroy(gameObject);
+        }
+
+
+        if (isLaser)
+        {
+           Destroy(gameObject);
+           Destroy(other.gameObject);
+
+        }
+
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y <= -7f)
+        {
+            transform.position = new Vector3(Random.Range(0.0f, 8.0f), 7f, 0);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,10 +14,10 @@ public class Player : MonoBehaviour
     private float _fireRate = 0.5f;
     private float _nextFire = 0.0f;
 
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        
     }
 
     // Update is called once per frame
