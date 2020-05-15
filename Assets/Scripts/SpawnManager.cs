b7dfb32b78e00742390c6d1a794670365fe27292
1forh <1forhlol@gmail.com>
2020-05-14 23:35:58 -0400

    updates

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,32 +7,37 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemeyPrefab;
     [SerializeField]
+    private GameObject _tripleShotPowerupPrefab;
+    [SerializeField]
     private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _powerupContainer;
 
     private bool _stopSpawning = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(SpawnRoutine());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
-    IEnumerator SpawnRoutine()
+    IEnumerator SpawnEnemyRoutine()
     {
         while (_stopSpawning == false)
         {
             Vector3 positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, 0);
             GameObject newEnemy = Instantiate(_enemeyPrefab, positionToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(2.0f);
         }
 
+    }
+
+    IEnumerator SpawnPowerupRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            float waitTime = Random.Range(10.0f, 20.0f);
+            Vector3 positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, 0);
+            GameObject newPowerup = Instantiate(_tripleShotPowerupPrefab, positionToSpawn, Quaternion.identity);
+            newPowerup.transform.parent = _powerupContainer.transform;
+            yield return new WaitForSeconds(waitTime);
+        }
+        
     }
 
     public void OnPlayerdeath()
