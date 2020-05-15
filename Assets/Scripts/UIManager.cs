7d7b88ab37d75ba1eb118ac668d7f594989998e1
1forh <1forhlol@gmail.com>
2020-05-15 14:39:57 -0400

    updates

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -12,6 +13,11 @@ public class UIManager : MonoBehaviour
     private Image _livesImage; 
     [SerializeField]
     private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
+    [SerializeField]
+    private Text _restartText;
+    private bool _gameIsOver = false;
 
 
     // Start is called before the first frame update
@@ -19,6 +25,19 @@ public class UIManager : MonoBehaviour
     {
         // assign text component to the handle
         _scoreText.text = "Score: " + 0;
+    }
+
+    void Update()
+    {
+        if (_gameIsOver == true)
+        {
+            bool _pressingResetKey = Input.GetKeyDown(KeyCode.R);
+
+            if (_pressingResetKey)
+            {
+                ResetGame();
+            }
+        }
     }
 
     public void UpdateScore(int score)
@@ -29,5 +48,28 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _livesImage.sprite = _liveSprites[currentLives];
+
+        if (currentLives == 0)
+        {
+            _gameIsOver = true;
+            StartCoroutine(ToggleGameoverText(currentLives));
+            _restartText.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator ToggleGameoverText(int currentLives)
+    {
+        while (currentLives == 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(.5f);
+            _gameOverText.gameObject.SetActive(false);
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    private void ResetGame()
+    {
+        SceneManager.LoadScene("Main_Menu");
     }
 }
